c7ce311da6709c4fa9abb03377165442244396c7
E177514A <cindy.ferrao@etu.univ-nantes.fr>
2018-12-28 15:46:00 +0100

    Correction d'indentation + commentaires français (utiles pour la compréhension en cours de projet, on les enlèvera à la fin si besoin)

--- src/langModel/Vocabulary.java
+++ src/langModel/Vocabulary.java
@@ -11,7 +11,7 @@ import java.util.TreeSet;
 
 /**
  * Class Vocabulary: class implementing the interface VocabularyInterface.
- * 
+ *
  * @author ... (2017)
  *
  */
@@ -19,68 +19,52 @@ public class Vocabulary implements VocabularyInterface {
 	/**
 	 * The set of words corresponding to the vocabulary.
 	 */
+	// set : ensemble d'objets sans doublons
 	protected Set<String> vocabulary;
 
-	
+
 	/**
 	 * Constructor.
 	 */
+	// treeset : obtenir un ensemble trié
 	public Vocabulary(){
-		this.vocabulary= new TreeSet<String>();
-		}
+		this.vocabulary = new TreeSet<String>();
+	}
 
-	/**
-	 *
-	 * @return int Size of the Set Vocabulary
-	 */
+
 	@Override
+	// retourne la taille de l'ensemble
 	public int getSize() {
 		return this.vocabulary.size();
 	}
 
-	/**
-	 * @return Set return all the vocabulary
-	 */
 	@Override
+	// retourne les mots contenu dans l'ensemble
 	public Set<String> getWords() {
 		return this.vocabulary;
 	}
 
-	/**
-	 *
-	 * @param word the word to find in the vocabulary.
-	 * @return false is Vocabulary don't contain the word
-	 * true if the vocabulary contains the word
-	 */
 	@Override
+	// retourne si le mot est dans l'ensemble ou non
 	public boolean contains(String word) {
 		return this.vocabulary.contains(word);
 	}
 
-	/**
-	 * Add a word in the Vocabulary
-	 * @param word the word to add.
-	 */
 	@Override
+	// ajoute un mot dans l'ensemble
 	public void addWord(String word) {
 		this.vocabulary.add(word);
 	}
 
-	/**
-	 * Remove a word from the Vocabulary
-	 * @param word the word to remove.
-	 */
 	@Override
+	// supprime un mot de l'ensemble
 	public void removeWord(String word) {
 		this.vocabulary.remove(word);
-		
 	}
 
-	/**
-	 * Add a list of Words in the vocabulary from a Set
-	 * @param ngramSet the set of n-grams whose words to add to the vocabulary.
-	 */
 	@Override
+	// analyse l'ensemble, liste les mots de chaque n-gramme puis les ajoute
+	// ngramSet : l'ensemble des n-grammes dont les mots doivent être ajoutés au vocabulaire
 	public void scanNgramSet(Set<String> ngramSet) {
 		for (String ngram : ngramSet)
 		{
@@ -88,49 +72,44 @@ public class Vocabulary implements VocabularyInterface {
 		}
 	}
 
-	/**
-	 * Add a list of Words in the vocabulary from a file
-	 * @param filePath the path of the file containing the vocabulary.
-	 */
 	@Override
+	// lis un fichier ayant un mot par ligne
+	// filePath : le chemin du fichier contenant le vocabulaire
 	public void readVocabularyFile(String filePath) {
-			Scanner fichier = null;
-			File essai = new File(filePath);
-				try {
-					fichier = new Scanner(essai);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			if(fichier!=null)
+		Scanner fichier = null;
+		File essai = new File(filePath);
+		try {
+			fichier = new Scanner(essai);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		if(fichier != null)
+		{
+			while(fichier.hasNextLine())
 			{
-				while(fichier.hasNextLine())
-				{
-					this.vocabulary.add(fichier.nextLine());
-				}
+				this.vocabulary.add(fichier.nextLine());
 			}
+		}
 	}
 
-	/**
-	 * Write the vocabulary in a file.
-	 * @param filePath the path of the file which contain the vocabulary.
-	 */
 	@Override
+	// écris un mot par ligne  dans un fichier
+	// filePath : le chemin du fichier contenant le vocabulaire.
 	public void writeVocabularyFile(String filePath) {
 		File fichier = new File(filePath);
-		FileWriter writer = null;
+		FileWriter writer;
 		try {
 			writer = new FileWriter(fichier);
-			
+
 			for(String mots : this.vocabulary)
 			{
 				writer.write(mots + "\n");
 			}
 			writer.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-		
-		
 
 }
